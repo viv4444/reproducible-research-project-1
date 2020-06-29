5681331723bb94fb2ce41f2209fe176df3d1d2b9
viv4444 <viveksingh.jma4@gmail.com>
2020-06-29 12:45:22 +0530

    Final update

--- weekend.R
+++ weekend.R
@@ -1,18 +1,14 @@
 #first we make a seperate column representing if the given day is weekend or weekday
 df$date <- as.Date(strptime(df$date, format="%Y-%m-%d"))
 df$datetype <- sapply(df$date, function(x) {
-  if (weekdays(x) == "Sábado" | weekdays(x) =="Domingo") 
-  {y <- "Weekend"} else 
-  {y <- "Weekday"}
+  if (weekdays(x) == "Saturday" | weekdays(x) =="Sunday") 
+    {y <- "Weekend"} 
+  else 
+    {y <- "Weekday"}
   y
 })
 #now we plot panel graphs giving info about steps taken per interval based on weekday/weekend info
 library(ggplot2)
 activity_by_date <- aggregate(steps~interval + datetype, df, mean, na.rm = TRUE)
-png("plot4.p.png")
-plot<- ggplot(activity_by_date, aes(x = interval , y = steps, color = datetype)) +
-  geom_line() +
-  labs(title = "Average daily steps by type of date", x = "Interval", y = "Average number of steps") +
-  facet_wrap(~datetype, ncol = 1, nrow=2)
-print(plot)
-dev.off()
+g<-qplot(x=interval,y=steps,data = activity_by_date,facets = .~datetype,geom = "line",group=1)
+print(g)
